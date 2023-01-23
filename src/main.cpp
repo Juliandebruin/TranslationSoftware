0ed15e8be164e348fffac1a2164f7c29ed8f536d
Julian de Bruin <jybruin@avans.nl>
2023-01-23 21:37:08 +0100

    Write test function to test data sending with spoofed data

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,14 @@ void setup() {
 	Serial.begin(9600);
 }
 
+void add(String key, String value) {
+	if (data.contains(key)) {
+		data.update(key, value);
+	} else {
+		data.add(key, value);				
+	}
+}
+
 void waitForData(String key) {
 	int startTime = millis();
 	bool dataReceived = false;
@@ -30,11 +38,7 @@ void waitForData(String key) {
 		String value = canComs.receive_message(&id);
 
 		if (id == RECEIVE_VALUE_ADDRESS) {
-			if (data.contains(key)) {
-				data.update(key, value);
-			} else {
-				data.add(key, value);				
-			}
+			add(key, value);
 		} else if (enlapsedDime > TIMEOUT) {
 			Serial.println("No data received after recieving a key!");
 			break;
@@ -54,7 +58,7 @@ String createJsonOfMap() {
 	return createJson.get_json_string();
 }
 
-void loop() {
+void translateData() {
 	int id = 0;
 	String key = canComs.receive_message(&id);
 
@@ -63,4 +67,23 @@ void loop() {
 		String json = createJsonOfMap();
 		uartComs.send_message(json);
 	}
+}
+
+void spoofMapData() {
+	add("rpm"    , "4000");
+	add("speed"  ,   "40");
+	add("battery",   "75");
+	add("pin"	 ,  "800");
+	add("pout"   ,  "900");
+}
+
+void testSendingJsonData() {
+	spoofMapData();
+	String json = createJsonOfMap();
+	uartComs.send_message(json);
+	delay(300);
+}
+
+void loop() {
+	testSendingJsonData();
 }
